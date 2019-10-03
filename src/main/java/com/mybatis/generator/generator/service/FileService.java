84b11dffc166e8fab7b481d05a6f520a72519391
lamymay <lamymay@outlook.com>
2019-10-03 08:05:32 +0800

    add service 0.1

---
+++ src/main/java/com/mybatis/generator/generator/service/FileService.java
@@ -0,0 +1,10 @@
+package com.mybatis.generator.generator.service;
+
+/**
+ * @author 叶超
+ * @since 2019/10/3 8:01
+ */
+public interface FileService {
+
+    boolean createService();
+}
---
+++ src/main/java/com/mybatis/generator/generator/service/FileServiceImpl.java
@@ -0,0 +1,23 @@
+package com.mybatis.generator.generator.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 叶超
+ * @since 2019/10/3 8:02
+ */
+@Slf4j
+@Service
+public class FileServiceImpl implements FileService {
+
+
+    @Override
+    public boolean createService() {
+        boolean flag = false;
+        //创建service文件
+
+        return flag;
+
+    }
+}
